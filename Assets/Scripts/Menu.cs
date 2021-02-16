903325d132552896d72200eaa9463512cd62c5c2
Evan <evanbowlen@maranathastudents.org>
2021-02-16 11:58:49 -0800

    More levels and a next level button (there are also the item sprites for next video)

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public void Replay()
+    public string nextLevel;
+
+    /*public void Replay()
     {
         FindObjectOfType<GameManager>().Reset();
+    }*/
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
     }
 
     public void QuitGame()
